9a83016f8b9300d82599672def60fe7288a59451
jmsmarcelo <jmsmarcelos@gmail.com>
2024-11-25 08:05:12 -0400

    add Task Repository

---
+++ src/TaskRepository.java
@@ -0,0 +1,43 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TaskRepository {
+    private static final String TASK_DATA_FILE = "task-data.json";
+    private static final String NEXT_TASK_ID_FILE = "next-task-id";
+    private Path nextTaskIdPath;
+
+    public synchronized boolean saveData(List<Task> tasks) throws IOException {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(TASK_DATA_FILE))) {
+            bw.write("[");
+            for(int i = 0; i < tasks.size(); i++) {
+                bw.newLine();
+                bw.write(tasks.get(i).toJson() + ( i < (tasks.size() - 1) ? "," : ""));
+            }
+            bw.newLine();
+            bw.write("]");
+        }
+        return false;
+    }
+    public long loadNextTaskId() {
+        long nextTaskId = 1;
+        try {
+            nextTaskIdPath = Paths.get(NEXT_TASK_ID_FILE);
+            String content = Files.readString(nextTaskIdPath).trim();
+            return content.matches("\\d+") ? Long.parseLong(content) : nextTaskId;
+        } catch (IOException e) {
+            return nextTaskId;
+        }
+    }
+    public synchronized void updateNextTaskId() {
+        try {
+            Files.writeString(nextTaskIdPath, String.valueOf(loadNextTaskId() + 1));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
