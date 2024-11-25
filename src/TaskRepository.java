264ceeb8bbae3911c0be88311c5ce76c4d13b062
jmsmarcelo <jmsmarcelos@gmail.com>
2024-11-25 08:16:01 -0400

    fix: resolve return state in saveData() method

--- src/TaskRepository.java
+++ src/TaskRepository.java
@@ -12,16 +12,21 @@ public class TaskRepository {
     private Path nextTaskIdPath;
 
     public synchronized boolean saveData(List<Task> tasks) throws IOException {
+        if(tasks == null) {
+            return false;
+        }
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(TASK_DATA_FILE))) {
             bw.write("[");
             for(int i = 0; i < tasks.size(); i++) {
                 bw.newLine();
-                bw.write(tasks.get(i).toJson() + ( i < (tasks.size() - 1) ? "," : ""));
+                bw.write("\t" + tasks.get(i).toJson() + ( i < (tasks.size() - 1) ? "," : ""));
             }
-            bw.newLine();
+            if(tasks.size() > 0) {
+                bw.newLine();
+            }
             bw.write("]");
+            return true;
         }
-        return false;
     }
     public long loadNextTaskId() {
         long nextTaskId = 1;
